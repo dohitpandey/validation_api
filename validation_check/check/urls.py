e4b4d14e1b3ba1b8fd5f709a88a0430698f6e9c0
Dohit Pandey <dohitp@gmail.com>
2023-05-17 02:17:19 +0530

    updated static url and cache timeout for swagger

--- validation_check/check/urls.py
+++ validation_check/check/urls.py
@@ -32,6 +32,6 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=100), name='schema-swagger-ui'),
     path('valid/', views.Validation.as_view())
 ]
