4e4ae8c0d91e393bb292d916be1f9b1a064b0986
Dohit Pandey <dohitp@gmail.com>
2023-05-16 12:34:56 +0530

    update req.txt

--- validation_check/validation_check/wsgi.py
+++ validation_check/validation_check/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'validation_check.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'validation_check.validation_check.settings')
 
 application = get_wsgi_application()
